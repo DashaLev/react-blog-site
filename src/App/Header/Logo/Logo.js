3976126cb681ce72383a53639dab8cf336f0d0b5
Dasha <d.l92@ukr.net>
2021-03-01 20:47:18 +0200

    added humberger menu changed hover on buttons

--- src/App/Header/Logo/Logo.js
+++ src/App/Header/Logo/Logo.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
 import logo from './logo.png'
+import logo_m from './logo_m.png'
+
 import './logo.css'
 
 const Logo = () => {
     return (
         <div className="logo">
-            <img src={logo} alt=""/>
+            <img src={logo} alt="logo" class="hide-xs"/>
+            <img src={logo_m} alt="logo" class="hide-md"/>
         </div>        
     )
 }
--- src/App/Header/Menu/Menu.js
+++ src/App/Header/Menu/Menu.js
@@ -1,26 +1,57 @@
-import React from 'react'
-
+import React, {Component} from 'react'
 import "./menu.css"
 
-const Menu = () => {
-    return (           
-        <ul  className="menu">
-            <li><a href="">HOME</a></li>
-            <li className="arrow"><a href="">CATEGORIES</a>
-                <ul class="submenu">
-                    <li><a href="">TRAVEL</a></li>
-                    <li><a href="">FASHION</a></li>
-                    <li><a href="">LIFESTYLE</a></li>
-                    <li><a href="">FOOD</a></li>
-                    <li><a href="">INSPIRATION</a></li>
-                    <li><a href="">PHOTOGRAPHY</a></li>
+class Menu extends Component {
+
+    state = {
+        mainMenu:"menu navigation hiden-menu",
+        mobMenu:"nav-mobile-menu",
+    }
+
+    openMenu = () => {
+        this.setState(() => ({
+            mainMenu: "menu navigation hiden-menu active-menu",
+            mobMenu:"nav-mobile-menu active-menu",
+        }))
+    }
+    closeMenu = () => {
+        this.setState(() => ({
+            mainMenu: "menu navigation hiden-menu",
+            mobMenu:"nav-mobile-menu",
+        }))
+    }
+    
+    render () {
+        return ( 
+            <nav>        
+                <ul className={this.state.mainMenu}>
+                    <button className="close-mob-menu"
+                    onClick={() => this.closeMenu()}>x</button>
+                    <li><a href="">HOME</a></li>
+                    <li className="arrow"><a href="">CATEGORIES</a>
+                        <ul className="submenu">
+                            <li><a href="">TRAVEL</a></li>
+                            <li><a href="">FASHION</a></li>
+                            <li><a href="">LIFESTYLE</a></li>
+                            <li><a href="">FOOD</a></li>
+                            <li><a href="">INSPIRATION</a></li>
+                            <li><a href="">PHOTOGRAPHY</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="">BLOG</a></li>
+                    <li><a href="">ABOUT</a></li>
+                    <li><a href="">CONTACT</a></li>
                 </ul>
-            </li>
-            <li><a href="">BLOG</a></li>
-            <li><a href="">ABOUT</a></li>
-            <li><a href="">CONTACT</a></li>
-        </ul>  
-    )
+                <div className={this.state.mobMenu}
+                     onClick={() => this.openMenu()}>
+                     
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </nav> 
+        )
+    }
 }
 
 export default Menu
